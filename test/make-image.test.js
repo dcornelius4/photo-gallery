04b54aecdb0d31b7cbbd21f920da346af5bcd4a8
Dave Cornelius <d_cornelius94@yahoo.com>
2019-02-26 16:20:26 -0800

    changed into template literal notation

--- test/make-image.test.js
+++ test/make-image.test.js
@@ -5,14 +5,11 @@ const test = QUnit.test;
 test('create a template literal function', assert => {
     //write the function in here and then move it into its own file
     function imageTemplate(image) {
-        // const html = `<li>
-        //     <h2>${image.title}</h2>
-        //     <img src="${image.url}">
-        // </li>`;
         const html = `<li>
-        <h2>UniWhal</h2>
-        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+            <h2>${image.title}</h2>
+            <img src="${image.url}">
 </li>`;
+     
         
         const template = document.createElement('template');
         //capitalize the HTML in innerHtml
